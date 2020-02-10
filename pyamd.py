975a55985bf2d044cd55824b61028440e770e1bf
Chih-Ming Lee <chihminglee1202@gmail.com>
2020-02-09 18:06:36 -0600

    fix bug

--- pyamd.py
+++ pyamd.py
@@ -142,8 +142,13 @@ md1 = """
 # entry
 - key 1: value 1
 - key 2: value 2 
-- key 3: NA
+
+# entry
+- key 3: value 3
+- key 4: value 4
+
 """
+
 p = parser.parse(md1)
 print(p)
 print('===============================================')
